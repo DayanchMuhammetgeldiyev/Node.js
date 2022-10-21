58c970e4c6783d91435f2ed6cc80378da87e51a0
DayanchMuhammetgeldiyev <66455000+DayanchMuhammetgeldiyev@users.noreply.github.com>
2022-10-21 15:31:37 +0300

    buffer

--- AsyncHooks/AsyncHooks.js
+++ AsyncHooks/AsyncHooks.js
@@ -1,2 +1,25 @@
 // The node:async_hooks module provides an API to track asynchronous resources. It can be accessed using
 // import async_hooks from 'node:async_hooks';
+import { createServer } from 'node:http';
+import {
+    executionAsyncId,
+    executionAsyncResource,
+    createHook
+} from 'async_hooks';
+const sym = Symbol('state'); // Private symbol to avoid pollution
+
+createHook({
+    init(asyncId, type, triggerAsyncId, resource) {
+        const cr = executionAsyncResource();
+        if (cr) {
+            resource[sym] = cr[sym];
+        }
+    }
+}).enable();
+
+const server = createServer((req, res) => {
+    executionAsyncResource()[sym] = { state: req.url };
+    setTimeout(function () {
+        res.end(JSON.stringify(executionAsyncResource()[sym]));
+    }, 100);
+}).listen(3000);
---
+++ Buffer/buffer.js
@@ -0,0 +1,30 @@
+import { Buffer } from "node:buffer"
+
+// const buf1 = Buffer.allocUnsafe(10)
+
+// < Buffer 01 02 03 >
+// const buf1 = Buffer.from([1, 2, 3])
+// console.log(buf1)
+
+// <Buffer 01 01 01 01>
+// const buf5 = Buffer.from([257, 257.5, -255, '1']);
+// console.log(buf5)
+
+// <Buffer 74 c3 a9 73 74> (in hexadeicmal notation)
+// const buf6 = Buffer.from('tést');
+// console.log(buf6);
+
+
+const buf = Buffer.from('hello world', 'utf8');
+console.log(buf.toString('hex'))
+
+// Create buffer length 10 number
+
+// The character encodings currently supported by Node.js are the following:
+
+// 'utf8'(alias: 'utf-8'): Multi - byte encoded Unicode characters.Many web pages and other document formats use UTF - 8. This is the default character encoding.When decoding a Buffer into a string that does not exclusively contain valid UTF - 8 data, the Unicode replacement character U + FFFD � will be used to represent those errors.
+
+// 'utf16le'(alias: 'utf-16le'): Multi - byte encoded Unicode characters.Unlike 'utf8', each character in the string will be encoded using either 2 or 4 bytes.Node.js only supports the little - endian variant of UTF - 16.
+
+// 'latin1': Latin - 1 stands for ISO - 8859 - 1. This character encoding only supports the Unicode characters from U + 0000 to U + 00FF.Each character is encoded using a single byte.Characters that do not fit into that range are truncated and will be mapped to characters in that range.
+
