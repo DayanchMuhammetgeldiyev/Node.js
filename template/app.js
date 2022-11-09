73de255095281921dfaef25e02dea721548a2e74
Dayanch Muhammetgeldiyev <66455000+DayanchMuhammetgeldiyev@users.noreply.github.com>
2022-11-10 00:22:02 +0300

    Classes method

--- template/app.js
+++ template/app.js
@@ -1,11 +1,23 @@
 import connection from "./db/db.js";
-import http from "node:http"
+import http from "node:http";
 
 
-connection.connect()
+class Server {
+  constructor() {
+    this.start();
+    this.dbconnect();
+  }
+  start() {
+    http
+      .createServer(function (req, res) {
+        res.write("Hello adasda!");
+        res.end();
+      })
+      .listen(3000);
+  }
+  dbconnect(){
+    connection.connect();
+ }
+}
 
-http.createServer(function (req, res) {
-    res.write('Hello adasda!');
-    res.end();
-}).listen(3000);
-
+new Server()
