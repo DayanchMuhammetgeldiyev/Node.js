f639920e6b9ad2c266879c521865eb996592d392
Dayanch Muhammetgeldiyev <66455000+DayanchMuhammetgeldiyev@users.noreply.github.com>
2022-11-10 20:57:49 +0300

    update

--- template/app.js
+++ template/app.js
@@ -4,6 +4,9 @@ import path from "node:path";
 import Express  from "express";
 import swig from "swig"
 const app = Express();
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
 
 class Server {
   constructor() {
@@ -21,7 +24,7 @@ class Server {
 
  initViewEngine() {
     app.engine('html', swig.renderFile);
-    app.set('views', path.join(__dirname, 'views', "index.html"));
+    app.set('views', path.join(__filename, 'views', "index.html"));
     app.set('view engine', 'html');
   }
 }
